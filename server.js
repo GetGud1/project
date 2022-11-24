bf1b3d58ca7b3993741d34c1c45bf5ccbfccb7ca
furqan <mfurqan264@gmail.com>
2022-11-24 22:24:48 +0800

    Initial Author

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser:true
@@ -16,13 +17,16 @@ db.on('error',error => console.error(error))
 db.once('open',() => console.log("connected to mongoose"))
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layouts')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended:false}))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
